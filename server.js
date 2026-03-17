da2f6cc72aa2738ad108de8b3b4854837d5c52b1
amisavani39 <amisavani43@gmail.com>
2026-03-17 12:33:33 +0530

    `Removed api/index.js and updated package.json to reflect changes`

--- server.js
+++ server.js
@@ -26,13 +26,9 @@ app.use("/api/v1/transcation", transaction);
 //app.get("/", (req, res) => res.send("Hello"));
 
 //build
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 
